0d45cde9daacc6e49c4ad6942c08feefafb5c3bf
xwasoux <oxwasouxo@gmail.com>
2022-09-14 08:59:03 +0900

    implementing IMU

--- Arduino/HWT905-TTL_IMU/src/main.cpp
+++ Arduino/HWT905-TTL_IMU/src/main.cpp
@@ -1,8 +1,14 @@
 #include "Arduino.h"
 #include <Arduino_FreeRTOS.h>
 #include "timers.h"
+#include <Wire.h>
+#include <JY901.h>
 
-
+struct IMU {
+	float accX = 0;
+	float accY = 0;
+	float accZ = 0;
+};
 
 
 void setup() {

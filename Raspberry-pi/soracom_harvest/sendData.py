d3baae03ea10346a1cca81fec166df6d96dc53ea
xwasoux <oxwasouxo@gmail.com>
2022-09-20 07:13:22 +0900

    sample implementing

--- Raspberry-pi/soracom_harvest/sendData.py
+++ Raspberry-pi/soracom_harvest/sendData.py
@@ -2,8 +2,34 @@ import sys
 import json
 import requests
 import time
+import datetime
 
-json_data = {"clock/10M": clock_n, "temp": temp_n}
-encode_json_data = json.dumps(json_data)
+headers={'Content-Type': 'application/json'}
+url = 'http://harvest.soracom.io'
 
-requests.post('http://harvest.soracom.io', data=encode_json_data, headers={'Content-Type': 'application/json'})
+def main():
+    interval_sec = 5 
+
+    payload = {}
+    payload['device_id'] = 'test'
+    payload['timestamp'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    while(True):
+        start_time = time.time()
+        
+        print('Data sending ...')
+        
+        try:
+            request = requests.post(url, data=json.dumps(payload), headers=headers, timeout=5)
+            print(request)
+        except requests.exceptions.ConnectTimeout:
+            print('ERROR: 接続がタイムアウトしました。"connect_air.sh" は実行していますか？')
+            if request.status_code == 400:
+                print('ERROR: データ送信に失敗しました。Harvest が有効になっていない可能性があります。')
+        
+        wait_time = start_time + interval_sec - start_time
+        if wait_time > 0:
+            time.sleep(wait_time)
+            
+if __name__ == '__main__':
+    main()

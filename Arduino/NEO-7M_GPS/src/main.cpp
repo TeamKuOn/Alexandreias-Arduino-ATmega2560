9f0a683c6448203367ef590dca19bf7a64201338
xwasoux <oxwasouxo@gmail.com>
2022-11-17 14:56:41 +0900

    苦肉の策

--- Arduino/NEO-7M_GPS/src/main.cpp
+++ Arduino/NEO-7M_GPS/src/main.cpp
@@ -20,14 +20,14 @@ void TaskCarPosition(void *pvParameters);
 void interruptHandler();
 /* The TinyGPS++ object */
 TinyGPSPlus gpsInfo;
-#define gpsSerial Serial2  // Rx -> Tx2(16), Tx -> Rx2(17)
+#define gpsSerial Serial2 // Rx -> Tx2(16), Tx -> Rx2(17)
 
 // structure setting
 struct NEO7M_GPS {
     // Date
-    uint32_t year = 0;
-    uint8_t month = 0;
-    uint8_t day = 0;
+    int year = 0;
+    int month = 0;
+    int day = 0;
 
     // Time
     uint8_t hour = 0;
@@ -38,20 +38,51 @@ struct NEO7M_GPS {
     double lat = 0;
     double lng = 0;
     double altMeters = 0;
+    double degree = 0;
+    double speedkmph = 0;
 
     // GPS hrizontal dilution of precision
     double hdop = 0;
 };
 
+
+struct SerialMesg {
+    char Date[40] = "";
+    char Time[40] = "";
+    
+    // // Accelaration XYZ
+    // char accX[20] = "";
+    // char accY[20] = "";
+    // char accZ[20] = "";
+
+    // GPS position
+    char lat[20] = "";
+    char lng[20] = "";
+    char altMeters[20] = "";
+    char degree[20] = "";
+    char speedkmph[20] = "";
+
+    // GPS hrizontal dilution of precision
+    char hdop[20] = "";
+
+        // Battery
+    char voltage[20] = "";
+    char current[20] = "";
+    char power[20] = "";
+
+};
+
+
 // global variable
 struct NEO7M_GPS carPosition;
+struct SerialMesg Mesg4Raspi;
 
 void setup() {
     Serial.begin(115200);
 
     pinMode(2, INPUT_PULLUP);
 
-    if ((xSerialSemaphore = xSemaphoreCreateMutex()) != NULL) {
+    if((xSerialSemaphore = xSemaphoreCreateMutex()) != NULL) {
         xSemaphoreGive((xSerialSemaphore));
     }
 
@@ -71,47 +102,72 @@ void interruptHandler() { xSemaphoreGiveFromISR(xInterruptSemaphore, NULL); }
 void TaskCarPosition(void *pvParameters) {
     gpsSerial.begin(GPSBaud);
 
-    for (;;) {
+    for(;;) {
         // if(xSemaphoreTake(xInterruptSemaphore, (TickType_t)10) == pdTRUE) {
-        if (xSemaphoreTake(xSerialSemaphore, (TickType_t)10) == pdTRUE) {
-            while (gpsSerial.available() > 0) {
+        if(xSemaphoreTake(xSerialSemaphore, (TickType_t)10) == pdTRUE) {
+            while(gpsSerial.available() > 0) {
                 gpsInfo.encode(gpsSerial.read());
             }
             xSemaphoreGive(xSerialSemaphore);
         }
         // }
 
-        Serial.println("---------------------------------------------------");
 
         if (xSemaphoreTake(xSerialSemaphore, (TickType_t)10) == pdTRUE) {
-            carPosition.year = gpsInfo.date.year();
-            carPosition.month = gpsInfo.date.month();
-            carPosition.day = gpsInfo.date.day();
-
             carPosition.hour = gpsInfo.time.hour();
             carPosition.minute = gpsInfo.time.minute();
             carPosition.second = gpsInfo.time.second();
-
             carPosition.lat = gpsInfo.location.lat();
             carPosition.lng = gpsInfo.location.lng();
             carPosition.altMeters = gpsInfo.altitude.meters();
-            carPosition.hdop = gpsInfo.hdop.value();
+            carPosition.degree = gpsInfo.course.deg();
+            carPosition.speedkmph = gpsInfo.speed.kmph();
+            carPosition.hdop = gpsInfo.hdop.value();            
+
+            sprintf(Mesg4Raspi.Date, "gps.date:%04d-%02d-%02d", carPosition.year, carPosition.month, carPosition.day);
+            Serial.println(Mesg4Raspi.Date);
+
+            sprintf(Mesg4Raspi.Time, "gps.time:%02d:%02d:%02d", carPosition.hour, carPosition.minute, carPosition.second);
+            Serial.println(Mesg4Raspi.Time);
+
+            sprintf(Mesg4Raspi.lat, "gps.lat:%f", carPosition.lat);
+            Serial.println(Mesg4Raspi.lat);
+
+            sprintf(Mesg4Raspi.lng, "gps.lng:%f", carPosition.lng);
+            Serial.println(Mesg4Raspi.lng);
+
+            sprintf(Mesg4Raspi.altMeters, "gps.altM:%f", carPosition.altMeters);
+            Serial.println(Mesg4Raspi.altMeters);
+
+            sprintf(Mesg4Raspi.degree, "gps.degree:%f", carPosition.degree);
+            Serial.println(Mesg4Raspi.altMeters);
+
+            sprintf(Mesg4Raspi.speedkmph, "gps.speedkmph:%f", carPosition.speedkmph);
+            Serial.println(Mesg4Raspi.altMeters);
+
+            sprintf(Mesg4Raspi.hdop, "gps.hdop:%f", carPosition.hdop);
+            Serial.println(Mesg4Raspi.hdop);
+
 
             xSemaphoreGive(xSerialSemaphore);
         }
-        
-        Serial.println(carPosition.year);
-        Serial.println(carPosition.month);
-        Serial.println(carPosition.day);
 
-        Serial.println(carPosition.hour);
-        Serial.println(carPosition.minute);
-        Serial.println(carPosition.second);
+        // if(xSemaphoreTake(xSerialSemaphore, (TickType_t)10) == pdTRUE) {
+        //     Serial.println(carPosition.year);
+        //     Serial.println(carPosition.month);
+        //     Serial.println(carPosition.day);
 
-        Serial.println(carPosition.lat, 8);
-        Serial.println(carPosition.lng, 8);
-        Serial.println(carPosition.altMeters);
-        Serial.println(carPosition.hdop);
+        //     Serial.println(carPosition.hour);
+        //     Serial.println(carPosition.minute);
+        //     Serial.println(carPosition.second);
+
+        //     Serial.println(carPosition.lat, 8);
+        //     Serial.println(carPosition.lng, 8);
+        //     Serial.println(carPosition.altMeters);
+        //     Serial.println(carPosition.hdop);
+
+        //     xSemaphoreGive(xSerialSemaphore);
+        // }
 
         vTaskDelay(1);
     }
